60162a1aba3731518c4244bd9f3fceef95849df9
arkebuzer <arkebuzer@yandex.ru>
2017-04-23 01:12:15 +0300

    Tests for models and enums added.

--- meteo/tests.py
+++ meteo/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+# from django.urls import reverse
 
-# Create your tests here.
+from meteo.models import Meteo
+from utils.enums import TimePointEnum
+
+
+class MeteoModelTests(TestCase):
+
+    def test_str(self):
+        meteo = Meteo(temperature=21.97, pressure=743.24, humidity=29)
+        self.assertEqual(str(meteo), '21.97 ℃, 743.24 mmHg, 29 %')
+
+
+class TimePointEnumTests(TestCase):
+    def test_str(self):
+        self.assertEqual(str(TimePointEnum.HOUR), 'HOUR')
+        self.assertNotEqual(str(TimePointEnum.MONTH), 'hour')
+
+    def test_equals_to_str(self):
+        self.assertTrue(TimePointEnum.HOUR.equals_to_str('HOUR'))
+        self.assertTrue(TimePointEnum.DAY.equals_to_str('day'))
+        self.assertTrue(TimePointEnum.MONTH.equals_to_str('mOnTh'))
+        self.assertFalse(TimePointEnum.HOUR.equals_to_str('MONTH'))
